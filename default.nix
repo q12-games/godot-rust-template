2cc4ac0abb7ad40162a896710fad5a0fc68d8d7d
Akshay Nair <phenax5@gmail.com>
2021-08-30 21:59:11 +0530

    init commit with boilerplate shit

---
+++ default.nix
@@ -0,0 +1,24 @@
+let
+  nixpkgs = import <nixpkgs> {};
+  rust_pkgs = nixpkgs.rust.packages.stable;
+in
+  with nixpkgs;
+  mkShell rec {
+    buildInputs = [
+      rust_pkgs.cargo
+      rust_pkgs.rustc
+      rust_pkgs.rustfmt
+      rust_pkgs.clippy
+      rust-analyzer
+
+      godot-headless
+      libclang
+    ];
+
+    nativeBuildInputs = [ clang pkgconfig ];
+
+    #RUST_BACKTRACE = 1;
+    LIBCLANG_PATH = "${libclang.lib}/lib";
+    RUST_SRC_PATH = rust_pkgs.rustPlatform.rustLibSrc;
+    LD_LIBRARY_PATH = lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
+  }
